631cc370772895b77dbec135da8465923ee493ec
Steven Kay <stevendkay@gmail.com>
2018-12-24 21:00:19 +0000

    now doing countdown calcs

--- client/public/js/bundle.js
+++ client/public/js/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("document.addEventListener('DOMContentLoaded', () => {\n  console.log(\"DOM has loaded\");\n});\n\n\n//# sourceURL=webpack:///./client/src/app.js?");
+eval("const onSecond = function() {\n  const target = new Date(2019,2,29,23,0,0);\n  const now = new Date();\n\n  const diff = target - now;\n  const diff_secs = diff/1000.0;\n  const diff_mins = diff_secs/60.0;\n  const diff_hours = diff_mins/60.0;\n  const diff_days = diff_hours/24.0;\n\n  // whole days\n  const whole_days = parseInt(diff_days);\n\n  const fractional_days = diff_days - whole_days;\n\n  // whole hours\n  const whole_hours = parseInt(((fractional_days)*86400)/(60.0*60.0));\n\n  const diffAfterHours = diff_secs - ((whole_days*86400)+(whole_hours*3600))\n  const whole_mins = parseInt(diffAfterHours/60.0);\n\n  const whole_seconds = parseInt(diffAfterHours - (whole_mins*60.0));\n  console.log(`${whole_days} Days ${whole_hours} Hours ${whole_mins} Mins ${whole_seconds} secs`);\n\n}\n\ndocument.addEventListener('DOMContentLoaded', () => {\n  console.log(\"DOM has loaded\");\n  const t=setInterval(onSecond,1000);\n});\n\n\n//# sourceURL=webpack:///./client/src/app.js?");
 
 /***/ })
 
--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,30 @@
+const onSecond = function() {
+  const target = new Date(2019,2,29,23,0,0);
+  const now = new Date();
+
+  const diff = target - now;
+  const diff_secs = diff/1000.0;
+  const diff_mins = diff_secs/60.0;
+  const diff_hours = diff_mins/60.0;
+  const diff_days = diff_hours/24.0;
+
+  // whole days
+  const whole_days = parseInt(diff_days);
+
+  const fractional_days = diff_days - whole_days;
+
+  // whole hours
+  const whole_hours = parseInt(((fractional_days)*86400)/(60.0*60.0));
+
+  const diffAfterHours = diff_secs - ((whole_days*86400)+(whole_hours*3600))
+  const whole_mins = parseInt(diffAfterHours/60.0);
+
+  const whole_seconds = parseInt(diffAfterHours - (whole_mins*60.0));
+  console.log(`${whole_days} Days ${whole_hours} Hours ${whole_mins} Mins ${whole_seconds} secs`);
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM has loaded");
+  const t=setInterval(onSecond,1000);
 });
