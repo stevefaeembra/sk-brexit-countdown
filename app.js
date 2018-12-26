3dab6fceef4574c8cd5334b4593aca4be67172f8
Steven Kay <stevendkay@gmail.com>
2018-12-26 10:40:48 +0000

    removed bodyparser ref

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require('path');
 const publicPath = path.join(__dirname, './client/public');
 
 app.use('/', express.static(publicPath));
-app.use(bodyParser.json());
 const port = process.env.PORT || 3000
 
 
