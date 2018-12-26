9dac7cfadc465913bf76103963f1f6f8520ef98f
Steven Kay <stevendkay@gmail.com>
2018-12-26 11:01:40 +0000

    changed public folder to ./client

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const publicPath = path.join(__dirname, '.');
+const publicPath = path.join(__dirname, './client');
 
 app.use('/', express.static(publicPath));
 const port = process.env.PORT || 3000
