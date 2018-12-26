c5ce02cbfb444d1929791a5940ce32b14dfc1726
Steven Kay <stevendkay@gmail.com>
2018-12-26 10:58:50 +0000

    got working locally, need to tweak public dir

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const publicPath = path.join(__dirname, './client/public');
+const publicPath = path.join(__dirname, '.');
 
 app.use('/', express.static(publicPath));
 const port = process.env.PORT || 3000
 
 app.get("/", (req,res) => {
-  res.sendFile(path.join('./client/index.html'));
+  res.sendFile(path.join(__dirname, './client/index.html'));
 });
 
 app.listen(port, function () {
