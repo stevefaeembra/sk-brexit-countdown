d54cf1d7bcdebfb6a45624ece8b9642ad100c7d2
Steven Kay <stevendkay@gmail.com>
2018-12-26 10:30:51 +0000

    try making app.js a simple server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.join(__dirname, './client/public');
+
+app.use('/', express.static(publicPath));
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000
+
+
+app.listen(port, function () {
+  console.log(`Started server on port ${port}`);
+});
